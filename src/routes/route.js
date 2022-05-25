cc77d010a282e2758a7a3d3e8134732747e2d945
Nirajray <nirajray5555@gmail.com>
2022-05-25 10:55:12 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router(); //used express to create route handlers
 
 const userController = require("../controllers/userController")
+const mid = require("../middleware/authe")
 
 router.post("/register", userController.createUser)
 router.post("/login", userController.login)
 
-router.put("/user/:userId/profile", userController.updateUser)
+router.put("/user/:userId/profile",mid.authentication,mid.authorization1 ,userController.updateUser)
 
 module.exports = router;
